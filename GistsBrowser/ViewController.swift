04422fc2a284ce50cfcd8820fdc2b12c066eaab6
tr.nasyrov <tr.nasyrov@tensor.ru>
2019-02-22 21:23:24 +0500

    Initial Commit

---
+++ GistsBrowser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GistsBrowser
+//
+//  Created by Тимур Насыров on 22/02/2019.
+//  Copyright © 2019 Timur Nasyrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
